41e403ee8e49e75290f74feb56e9878417b14b50
qiaowei <jov123@163.com>
2015-07-21 16:38:56 +0800

    boss view

---
+++ Isaac/BossItemCell.h
@@ -0,0 +1,13 @@
+//
+//  BossItemCell.h
+//  Isaac
+//
+//  Created by Shuwei on 15/7/21.
+//  Copyright (c) 2015年 Shuwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BossItemCell : UICollectionViewCell
+
+@end
---
+++ Isaac/BossItemCell.m
@@ -0,0 +1,13 @@
+//
+//  BossItemCell.m
+//  Isaac
+//
+//  Created by Shuwei on 15/7/21.
+//  Copyright (c) 2015年 Shuwei. All rights reserved.
+//
+
+#import "BossItemCell.h"
+
+@implementation BossItemCell
+
+@end
