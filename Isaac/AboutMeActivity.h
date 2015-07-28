c9545722420eb0f5ec914e4918e132cd63728718
joveeth <jov123@163.com>
2015-07-28 20:04:25 +0800

    ss

---
+++ Isaac/AboutMeActivity.h
@@ -0,0 +1,13 @@
+//
+//  AboutMeActivity.h
+//  Isaac
+//
+//  Created by jov jov on 7/28/15.
+//  Copyright (c) 2015 Shuwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutMeActivity : UITableViewController
+
+@end
---
+++ Isaac/AboutMeActivity.m
@@ -0,0 +1,26 @@
+//
+//  AboutMeActivity.m
+//  Isaac
+//
+//  Created by jov jov on 7/28/15.
+//  Copyright (c) 2015 Shuwei. All rights reserved.
+//
+
+#import "AboutMeActivity.h"
+
+@interface AboutMeActivity ()
+
+@end
+
+@implementation AboutMeActivity
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
